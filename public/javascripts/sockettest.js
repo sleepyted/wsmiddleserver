62454fde21480eded4d46daa4261de38a93cde4c
matengfei <1341555203@qq.com>
2018-02-22 17:44:32 +0800

    add .gitignore

--- public/javascripts/sockettest.js
+++ public/javascripts/sockettest.js
@@ -1,7 +1,25 @@
 /**
  * Created by matengfei1 on 2018/2/6.
  */
+/**
+ * Client events
+connect：连接成功
+connecting：正在连接
+disconnect：断开连接
+connect_failed：连接失败
+error：错误发生，并且无法被其他事件类型所处理
+message：同服务器端message事件
+anything：同服务器端anything事件
+reconnect_failed：重连失败
+reconnect：成功重连
+reconnecting：正在重连
+**/
 var socket = io.connect('http://localhost:10000');
+
+socket.on('connect', function () {
+    console.log('connect')
+})
+
 socket.on('news', function (data) {
     console.log(data);
     // socket.emit('dot', { my: 'data' },function(data){
@@ -14,12 +32,15 @@ socket.on('news', function (data) {
 var btn = document.getElementById('btn')
 var dotData = document.getElementById('dotData')
 
-btn.addEventListener('click',function(e){
-    e.preventDefault()
+function emitDotEvent() {
     console.log('-emit dot data-')
-    socket.emit('dot', dotData.value , function(data){
-        if (data){
+    socket.emit('dot', dotData.value, function (data) {
+        if (data) {
             console.log('-dot data sent success-')
         }
     })
+}
+btn.addEventListener('click',function(e){
+    e.preventDefault()
+    emitDotEvent();
 })
